00e9c57896fbbc956b7bc1e30a090bcee3a2c3f4
Victor Makinde <zigamakinde@gmail.com>
2025-06-28 19:13:46 +0100

    Db Updated

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from app import create_app
 
 # Create the Flask application instance
 app = create_app()
-
+    
 if __name__ == "__main__":
     logging.info("Flask app started")
     # Run the Flask development server
